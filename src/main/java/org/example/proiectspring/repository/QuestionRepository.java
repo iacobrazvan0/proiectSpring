5546da4454129c285d2b671544387c49c626f847
Iacob Razvan <91972381+iacobrazvan0@users.noreply.github.com>
2025-01-11 15:01:48 +0200

    Delete src directory

--- src/main/java/org/example/proiectspring/repository/QuestionRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.proiectspring.repository;
-
-import org.example.proiectspring.model.Question;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuestionRepository extends JpaRepository<Question, Long> {
-}
